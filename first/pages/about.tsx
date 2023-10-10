2a16fae07f4f6ff93da2b40cedf55246808691e6
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-10-11 01:09:55 +0330

    1. routing in static way

---
+++ first/pages/about.tsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
+    >
+      <div>this is my about page</div>
+    </main>
+  );
+};
+
+export default About;
