8a627eeee72c6fdcc29b7997bba926ad34e391d1
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-10-11 16:32:36 +0330

    5. link component and replace attr

--- first/pages/index.tsx
+++ first/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,46 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div>this is my home page</div>
+      <ul>
+        <li>
+          <Link href="about" legacyBehavior>
+            <a>about page</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="posts" legacyBehavior>
+            <a>posts page</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="posts/1" legacyBehavior>
+            <a>post - 1</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="blogs" legacyBehavior>
+            <a>main blog </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="blogs/with/params" legacyBehavior>
+            <a>blog with params</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/1" legacyBehavior>
+            <a>user with id - 1</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/1/review/1" legacyBehavior replace>
+            <a>
+              (back to last visited link #replace )user with id - 1 with review
+              id : 1
+            </a>
+          </Link>
+        </li>
+      </ul>
     </main>
   );
 }
