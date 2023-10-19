c23109ff1144b08849f40f1426234ff83bac8202
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-10-19 19:24:37 +0330

    24. signIN and signOUT button using next-auth

--- next-auth/pages/api/auth/[...nextauth].ts
+++ next-auth/pages/api/auth/[...nextauth].ts
@@ -1,27 +1,13 @@
 import NextAuth from "next-auth";
-import AppleProvider from "next-auth/providers/apple";
-import FacebookProvider from "next-auth/providers/facebook";
-import GoogleProvider from "next-auth/providers/google";
-import EmailProvider from "next-auth/providers/email";
-
-type providers = {
-  GoogleProvider: {
-    clientId: string | undefined;
-    clientSecret: string | undefined;
-  };
-};
-
-export default NextAuth({
+import type { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+export const authOptions: NextAuthOptions = {
   providers: [
-    // OAuth authentication providers...
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID || "",
-      clientSecret: process.env.GOOGLE_SECRET || "",
-    }),
-    // Passwordless / email sign in
-    EmailProvider({
-      server: process.env.MAIL_SERVER,
-      from: "NextAuth.js <no-reply@example.com>",
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
-});
+};
+const handler = NextAuth(authOptions);
+export default handler;
--- next-auth/pages/index.tsx
+++ next-auth/pages/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, signOut } from "next-auth/react";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -5,9 +6,11 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
     >
       <div>home page</div>
+      <button onClick={() => signIn("github")}>sign in</button>
+      <button onClick={() => signOut()}>sign out</button>
     </main>
   );
 }
