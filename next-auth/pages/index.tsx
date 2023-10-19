5eb085db5b06fea1c178e53536cc819a808986a5
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-10-19 19:42:11 +0330

    25. session and its status

--- next-auth/pages/index.tsx
+++ next-auth/pages/index.tsx
@@ -1,16 +1,25 @@
-import { signIn, signOut } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { Inter } from "next/font/google";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
     >
       <div>home page</div>
-      <button onClick={() => signIn("github")}>sign in</button>
-      <button onClick={() => signOut()}>sign out</button>
+      {session ? (
+        <>
+          <p>you are logged in as {session.user?.name}</p>
+          <button onClick={() => signOut()}>sign out</button>
+        </>
+      ) : (
+        <button onClick={() => signIn("github")}>sign in</button>
+      )}
     </main>
   );
 }
